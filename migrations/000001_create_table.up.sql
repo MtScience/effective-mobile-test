f35f0103cff12c8b7950101e51d650947527f02f
MtScience <107070506+MtScience@users.noreply.github.com>
2026-04-19 16:11:42 +0300

    Update 000001_create_table.up.sql

--- migrations/000001_create_table.up.sql
+++ migrations/000001_create_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS subscriptions (
     service VARCHAR(30) NOT NULL,
     price INT NOT NULL CHECK (price >= 0),
     subscribed_on DATE NOT NULL,
-    unsubscribed_on DATE CHECK (ubsubscribed_on >= subscribed_on)
+    unsubscribed_on DATE CHECK (unsubscribed_on >= subscribed_on)
 );
 
 CREATE INDEX IF NOT EXISTS idx_subscription_user_ids ON subscriptions (user_id);
